e234b7f95085074d3615ed8f131e5d8d66946fbf
jason <jk17@ualberta.ca>
2014-06-12 22:14:34 -0700

    init commit.

---
+++ index.js
@@ -0,0 +1,4 @@
+exports.provider = require('./lib/provider.js');
+exports.client = require('./lib/client.js');
+exports.transform = require('./lib/transform.js');
+exports.config = {client_scope : "wl.basic wl.emails wl.offline_access wl.skydrive_update"};
